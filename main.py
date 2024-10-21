e4d7acdeb8b9cc29541ed912ea3c175b95e2b942
HassanRasoo98 <hassanrasool1057@gmail.com>
2024-10-22 00:59:36 +0500

    preview removed

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import traceback
 import streamlit as st
-import os
 from pathlib import Path
-import time
 
 from downloader import *
 
@@ -12,24 +11,21 @@ st.title("YouTube Video/Audio Downloader")
 url = st.text_input("Enter the YouTube video URL:")
 
 # Output directory (using Streamlit's temporary directory)
-output_path = "downloads"
-Path(output_path).mkdir(parents=True, exist_ok=True)
+video_path = "video"
+audio_path = "audio"
+Path(video_path).mkdir(parents=True, exist_ok=True)
+Path(audio_path).mkdir(parents=True, exist_ok=True)
 
 # Buttons to download video or audio
 if st.button("Download Video"):
     if url:
         with st.spinner("Downloading video..."):
             try:
-                video_file = download_video(url, output_path=output_path)
+                video_file = download_video(url, output_path=video_path)
                 st.success(f"Video downloaded successfully!")
-                st.video(video_file)  # Show video in Streamlit
                 
-                # Clean up: remove video after showing it
-                time.sleep(3)  # Give a delay to allow user to see the video
-                if os.path.exists(video_file):
-                    os.remove(video_file)
-                    st.info(f"Video file removed from server.")
             except Exception as e:
+                traceback.print_exc()
                 st.error(f"Error: {e}")
     else:
         st.error("Please enter a valid URL.")
@@ -38,17 +34,9 @@ if st.button("Download Audio"):
     if url:
         with st.spinner("Downloading audio..."):
             try:
-                audio_file = download_audio(url, output_path=output_path)
+                audio_file = download_audio(url, output_path=audio_path)
                 st.success(f"Audio downloaded successfully!")
                 
-                # Provide download link for audio
-                generate_download_link(audio_file)
-                
-                # Optionally remove the audio file after download, or keep it for a while
-                time.sleep(60)  # Wait for 1 minute before removing
-                if os.path.exists(audio_file):
-                    os.remove(audio_file)
-                    st.info(f"Audio file removed from server.")
             except Exception as e:
                 st.error(f"Error: {e}")
     else:
